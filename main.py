455a7dfe436992f51cb848173aa366afc31e85fa
jsantore <jsantore@bridgew.edu>
2022-11-21 20:31:48 -0500

    recursive and loop factorial

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def loop_fact(n):
+    result = 1
+    for number in range(1,n):
+        result = result*number
+    return n*result
+
+def fact(n):
+    if n<=1:
+        return 1
+    return  n * fact(n-1)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    my_n = int(input("what number shall we calculate factorial:"))
+    result = loop_fact(my_n)
+    print(f"the result is {result}")
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+main()
